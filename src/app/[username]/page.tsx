d7da137d0959c1091e2515657aa7c553abbff1c2
Luan Felipe <lfps1999@gmail.com>
2023-07-22 00:27:23 -0300

    Creating create target

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,12 +1,19 @@
 "use client"
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import api from "@/configs/api";
 import { ITargetWithUser } from "@/interfaces/target";
 
 const Profile = ({ params }: { params: { username: string } }) => {
   const [userTargets, setUserTargets] = useState<ITargetWithUser[]>([]);
 
+  const [title, setTitle] = useState<string>("");
+  const [isPrivate, setIsPrivate] = useState<boolean>(false);
+
   useEffect(() => {
+    listAllTargets();
+  }, []);
+
+  const listAllTargets = () => {
     const token = localStorage.getItem("token");
     const headers = {
       Authorization: `Bearer ${token}`
@@ -16,10 +23,32 @@ const Profile = ({ params }: { params: { username: string } }) => {
     }).catch((err) => {
       console.error(err);
     });
-  }, []);
+  }
 
   const handleCompleteTarget = (isCompleted: string) => {
     console.log(isCompleted)
+  }
+
+  const handleCreateTarget = (event: FormEvent) => {
+    event.preventDefault();
+    
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
+
+    const data = {
+      title,
+      isPrivate
+    }
+
+    api.post(`/target/create`, data, { headers }).then((data) => {
+      listAllTargets();
+      setTitle("");
+      setIsPrivate(false);
+    }).catch((err) => {
+      console.error(err);
+    });
   }
 
   return (
@@ -32,7 +61,6 @@ const Profile = ({ params }: { params: { username: string } }) => {
         <nav className='grid-in-nav bg-blue-400'>
           <div className="h-full flex justify-center items-center flex-col">
             <a className='h-[2rem] w-1/2 bg-zinc-50 text-center' href={`/${params.username.toLowerCase()}`}>{params.username}</a>
-            {/* <button className='h-[2rem] w-1/2 bg-zinc-50 mt-1' onClick={handleSignOut}>Sair</button> */}
           </div>
         </nav>
         <main className='grid-in-main bg-yellow-300'>
@@ -47,7 +75,14 @@ const Profile = ({ params }: { params: { username: string } }) => {
                 </li>
               )
             })}
-            <li></li>
+            <li className={`bg-slate-300 hover:bg-slate-400 shadow-lg my-1 p-2 rounded-md border-2 border-gray-500 flex items-center`}>
+              <form onSubmit={handleCreateTarget}>
+                <input type="text" name='title' value={title} onChange={event => setTitle(event.target.value)}/>
+                <input type="checkbox" name="isPrivate" checked={isPrivate} onChange={event => setIsPrivate(event.target.checked)} />
+                { isPrivate ? <span className="bg-yellow-600">Privado</span> : <span className="bg-green-600">Publico</span> }
+                <button type='button' onClick={handleCreateTarget}>Enviar</button>
+              </form>
+            </li>
           </ul>
         </main>
       </div>
